92f5e5129d975c67e4afc78d73a2951158338fee
George Paulino <gpaulinoalmonte@outlook.com>
2024-08-14 00:29:23 -0400

    Nuevo ejercicio

---
+++ Modulo 1/pratica_string_2.py
@@ -0,0 +1,21 @@
+'''
+Práctica String 2
+Crea un código que imprima en pantalla la siguiente expresión.
+
+A	B	C
+D	E	F
+G	H	I
+Notas:
+
+- Observa que la distancia horizontal entre los números ha sido creada con UNA tabulación.
+
+- Puedes usar una o más declaraciones print
+
+Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+
+¿Aún tienes dudas acerca de por qué el módulo de evaluación no acepta tu respuesta? Hemos incluido al final de cada sección posibles respuestas a cada uno de los ejercicios. Confiamos en que puedas recurrir a ellas para detectar diferencias rápidamente, evitando que pierdas el tiempo o debas esperar por una respuesta a tu pregunta. Si aún así tienes dudas a resolver, puede ser una buena oportunidad para repasar la lección en cuestión, o utilizar el espacio de Preguntas y Respuestas que tienes a tu disposición. Muchos éxitos!
+'''
+
+print("A\t B\t C\t")
+print("D\t E\t F\t")
+print("G\t H\t I\t")
